4d358054a19440ac5d949688b6ed1cea3e15bb21
lewpar <lewis.o.parmenter@gmail.com>
2025-03-20 21:33:02 +1100

    Added examples for listening to both spot and update events at the same time using a hosted app.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using AssettoNet.Events;
-using AssettoNet.Network;
+﻿using AssettoNet.Example.Services;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AssettoNet.Example;
 
@@ -7,45 +9,14 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        var client = new AssettoClient();
+        var builder = Host.CreateApplicationBuilder(args);
 
-        client.OnClientConnected += Client_OnClientConnected;
-        client.OnClientHandshake += Client_OnClientHandshake;
-        client.OnClientListening += Client_OnClientListening;
-        client.OnClientOperationEvent += Client_OnClientOperationEvent;
+        builder.Services.AddHostedService<AssettoSpotService>();
+        builder.Services.AddHostedService<AssettoUpdateService>();
 
-        await client.ConnectAsync("localhost", 9996);
-        await client.ListenForEventsAsync(AssettoEventType.Spot);
-    }
+        var app = builder.Build();
 
-    private static void Client_OnClientListening(object? sender, EventArgs e)
-    {
-        Console.WriteLine("Listening for events..");
-    }
-
-    private static void Client_OnClientOperationEvent(object? sender, AssettoOperationEventArgs e)
-    {
-        Console.WriteLine("Received event: " + e.EventType);
-
-        switch (e.EventType)
-        {
-            case AssettoEventType.Spot:
-                Console.WriteLine(e.Spot?.ToString());
-                break;
-
-            case AssettoEventType.Update:
-                Console.WriteLine(e.Update?.ToString());
-                break;
-        }
-    }
-
-    private static void Client_OnClientHandshake(object? sender, AssettoHandshakeEventArgs e)
-    {
-        Console.WriteLine("Passed handshake.");
-    }
-
-    private static void Client_OnClientConnected(object? sender, EventArgs e)
-    {
-        Console.WriteLine("Connected");
+        await app.RunAsync();
+        await app.WaitForShutdownAsync();
     }
 }
---
+++ Services/AssettoSpotService.cs
@@ -0,0 +1,58 @@
+﻿using AssettoNet.Events;
+using AssettoNet.Network;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AssettoNet.Example.Services;
+
+internal class AssettoSpotService : BackgroundService
+{
+    private readonly AssettoClient _client;
+    private readonly ILogger<AssettoSpotService> _logger;
+
+    public AssettoSpotService(ILogger<AssettoSpotService> logger)
+    {
+        _client = new AssettoClient();
+
+        _client.OnClientConnected += Client_OnClientConnected;
+        _client.OnClientHandshake += Client_OnClientHandshake;
+        _client.OnClientListening += Client_OnClientListening;
+        _client.OnClientOperationEvent += Client_OnClientOperationEvent;
+
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await _client.ConnectAsync("localhost", 9996);
+        await _client.ListenForEventsAsync(AssettoEventType.Spot);
+    }
+
+    private void Client_OnClientListening(object? sender, EventArgs e)
+    {
+        _logger.LogInformation("Listening for events..");
+    }
+
+    private void Client_OnClientOperationEvent(object? sender, AssettoOperationEventArgs e)
+    {
+        _logger.LogInformation("Received event: " + e.EventType);
+
+        switch (e.EventType)
+        {
+            case AssettoEventType.Spot:
+                _logger.LogInformation(e.Spot?.ToString());
+                break;
+        }
+    }
+
+    private void Client_OnClientHandshake(object? sender, AssettoHandshakeEventArgs e)
+    {
+        _logger.LogInformation("Passed handshake.");
+    }
+
+    private void Client_OnClientConnected(object? sender, EventArgs e)
+    {
+        _logger.LogInformation("Connected");
+    }
+}
