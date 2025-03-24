42b56988e2e1f914f7af919914bb9b2c2f75927d
lewpar <lewis.o.parmenter@gmail.com>
2025-03-25 10:06:20 +1100

    Updated to match the recent changes to the AssettoNet library.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using AssettoNet.Example.Services;
-
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+﻿using AssettoNet.Network;
 
 namespace AssettoNet.Example;
 
@@ -9,14 +6,46 @@ internal class Program
 {
     static async Task Main(string[] args)
     {
-        var builder = Host.CreateApplicationBuilder(args);
+        var client = new AssettoClient();
 
-        builder.Services.AddHostedService<AssettoSpotService>();
-        builder.Services.AddHostedService<AssettoUpdateService>();
+        client.OnConnected += Client_OnConnected;
+        client.OnLapCompleted += Client_OnLapCompleted;
+        client.OnPhysicsUpdate += Client_OnPhysicsUpdate;
+        client.OnUnhandledException += Client_OnUnhandledException;
 
-        var app = builder.Build();
+        Console.WriteLine("Connecting to server..");
 
-        await app.RunAsync();
-        await app.WaitForShutdownAsync();
+        await client.ConnectAsync(host: "localhost", port: 9996);
+        await client.ListenForEventsAsync();
+
+        await Task.Delay(TimeSpan.FromSeconds(120));
+
+        await client.DisconnectAsync();
+    }
+
+    private static void Client_OnUnhandledException(object? sender, UnhandledExceptionEventArgs e)
+    {
+        var ex = (Exception)e.ExceptionObject;
+        Console.WriteLine($"{ex.Message}: {ex.StackTrace}");
+    }
+
+    private static void Client_OnPhysicsUpdate(object? sender, Events.AssettoPhysicsUpdateEventArgs e)
+    {
+        //Console.Clear();
+        //Console.WriteLine("[Physics Update]");
+        //Console.WriteLine($"{e.Data.ToString()}");
+    }
+
+    private static void Client_OnLapCompleted(object? sender, Events.AssettoLapCompletedEventArgs e)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"[Completed Lap {e.Data.Lap}]");
+        Console.WriteLine($"{e.Data.ToString()}");
+    }
+
+    private static void Client_OnConnected(object? sender, Events.AssettoConnectedEventArgs e)
+    {
+        Console.WriteLine("Connected.");
+        Console.WriteLine(e.Handshake.ToString());
     }
 }
